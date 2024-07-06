bc92f5e377bb517dd8b67d69ae11090927435a10
Saket Mugunda <saketmugunda123@gmail.com>
2024-07-06 00:23:34 -0500

    Remove Duplicates From Sorted LL

---
+++ LinkedLists/MultiplePointers/RemoveDuplicatesFromSortedLinkList.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return head
+    
+    
+        left = head
+        right = head.next
+        
+        while right != None:
+            
+            if left.val == right.val:
+                left.next = right.next
+                right = right.next
+                continue
+            
+            left = left.next
+            right = right.next
+        
+        return head
