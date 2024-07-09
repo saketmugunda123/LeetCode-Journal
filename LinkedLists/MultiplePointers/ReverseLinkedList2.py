c59ba1c1ae9e7749025e5c788e48265ce794e936
Saket Mugunda <saketmugunda123@gmail.com>
2024-07-08 23:51:21 -0500

    2nd Reverse Linked List

---
+++ LinkedLists/MultiplePointers/ReverseLinkedList2.py
@@ -0,0 +1,59 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseBetween(self, head, left, right):
+        """
+        :type head: ListNode
+        :type left: int
+        :type right: int
+        :rtype: ListNode
+        """
+        
+        if head == None:
+            return head
+        
+        dummy = ListNode(0, head)
+        
+        prev = dummy
+        curr = head
+        pos =1
+        while pos != left:
+            prev = curr
+            curr = curr.next
+            pos +=1
+        
+        leftBound = curr
+   
+        nxtNode = curr.next
+        pos +=1
+        while pos <= right:
+            #store next node
+            nxtNode2 = nxtNode.next
+    
+            nxtNode.next = curr 
+            curr = nxtNode
+            nxtNode = nxtNode2
+            pos +=1
+        
+        
+        
+        prev.next = curr
+        leftBound.next = nxtNode
+    
+       
+        
+        return dummy.next
+        
+            
+            
+        
+        
+            
+        
+        
+        
+        
+            
