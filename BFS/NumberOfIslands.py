0d088df0dcd898fc3b6a114fdc36acd79f94d595
Saket Mugunda <saketmugunda123@gmail.com>
2024-11-18 14:38:49 -0600

    BFS implementation

--- BFS/NumberOfIslands.py
+++ BFS/NumberOfIslands.py
@@ -5,4 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
+         if not grid:
+            return 0
         
+        #create the BFS function
+        def BFS(row, col):
+            queue = [(row,col)]
+            grid[row][col] = '0' #marking node as visited
+            while queue:
+                currRow, currCol = queue.pop(0)
+                directions = [(1,0), (-1,0),(0,1), (0,-1)] #getting neighbors
+                for rowDirect, colDirect in directions:
+                    newRow, newCol = currRow + rowDirect, currCol + colDirect
+                    if(newRow >= 0 and newRow < len(grid) and newCol >=0 and newCol < len(grid[0]) and grid[newRow][newCol] == '1'):
+                        queue.append((newRow,newCol)) #valid neighbor
+                        grid[newRow][newCol] = '0' #mark as visited
+                    
