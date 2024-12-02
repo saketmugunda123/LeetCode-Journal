7b9a4b6d80a44058c94da4af296bc74f05a99f13
Saket Mugunda <saketmugunda123@gmail.com>
2024-12-02 09:58:39 -0600

    Create base case

--- Trees/DepthFirstSearch/InOrderTraversal.py
+++ Trees/DepthFirstSearch/InOrderTraversal.py
@@ -1,3 +1,10 @@
 class Solution(object):
     def inorderTraversal(self, root):
-        
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        def inOrder(root):
+            answer = []
+            if not root:
+                return []
